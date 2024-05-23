6ff7b4ce3f06c9c0adf4a0b84c792cdac206476b
asif <asifrahman00010@gmail.com>
2024-05-24 01:58:34 +0600

    done testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,15 +36,25 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation("com.google.firebase:firebase-database:20.3.1")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("androidx.test.espresso:espresso-intents:3.5.1")
+    testImplementation("androidx.arch.core:core-testing:2.1.0")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito:mockito-core:3.9.0")
     testImplementation("androidx.test.ext:junit:1.1.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.uiautomator:uiautomator:2.2.0")
+    androidTestImplementation("org.mockito:mockito-android:3.9.0")
+
+    testImplementation ("androidx.arch.core:core-testing:2.1.0")
         // JUnit
         testImplementation("junit:junit:4.13.2")
     testImplementation ("org.junit.jupiter:junit-jupiter:5.8.1")
@@ -55,6 +65,8 @@ dependencies {
     androidTestImplementation ("androidx.test:rules:1.5.0")
 
         // Mockito for mocking
+      //  testImplementation("org.mockito:mockito-core:3.11.2")
+     //   androidTestImplementation("org.mockito:mockito-android:3.11.2")
     testImplementation ("org.mockito:mockito-core:4.0.0")
     androidTestImplementation ("org.mockito:mockito-android:4.0.0")
 
